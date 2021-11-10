ffa1c4135651041674418de40c649b0e0a3e7627
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2021-11-10 14:13:52 +0500

    feat: Add cnavas_integration plugin

---
+++ src/ol_openedx_canvas_integration/README.rst
@@ -0,0 +1,5 @@
+ol_openedx_canvas_integration
+=============================
+
+
+A plugin that enables canvas integration within edX.
---
+++ src/ol_openedx_canvas_integration/urls.py
@@ -0,0 +1,17 @@
+"""
+Canvas Integration API endpoint urls.
+"""
+
+from django.conf.urls import url
+
+from ol_openedx_canvas_integration import views
+
+urlpatterns = [
+    url(r'^add_canvas_enrollments$',
+        views.add_canvas_enrollments, name="add_canvas_enrollments"),
+    url(r'^list_canvas_enrollments$',
+        views.list_canvas_enrollments, name="list_canvas_enrollments"),
+    url(r'^list_canvas_assignments$', views.list_canvas_assignments, name="list_canvas_assignments"),
+    url(r"^list_canvas_grades$", views.list_canvas_grades, name="list_canvas_grades"),
+    url(r"^push_edx_grades$", views.push_edx_grades, name="push_edx_grades"),
+]
