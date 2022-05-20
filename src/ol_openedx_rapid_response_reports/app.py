e6d7fb25a8b93aa23671ca74c878c0c8d0fb6d70
Arslan <arslan.ashraf@arbisoft.com>
2022-05-20 13:00:08 +0500

    feat: serve rapid_response_reports assets and add instructor tab from plugin

--- src/ol_openedx_rapid_response_reports/app.py
+++ src/ol_openedx_rapid_response_reports/app.py
@@ -3,11 +3,10 @@ ol_openedx_rapid_response_reports Django application initialization.
 """
 
 from django.apps import AppConfig
-from edx_django_utils.plugins import PluginContexts, PluginURLs
+from edx_django_utils.plugins import PluginContexts, PluginSettings, PluginURLs
+from lms.djangoapps.instructor.constants import INSTRUCTOR_DASHBOARD_PLUGIN_VIEW_NAME
 from openedx.core.constants import COURSE_ID_PATTERN
-from openedx.core.djangoapps.plugins.constants import ProjectType
-
-from ol_openedx_rapid_response_reports.constants import RAPID_RESPONSE_PLUGIN_VIEW_NAME
+from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
 
 class RapidResponsePluginConfig(AppConfig):
@@ -25,9 +24,16 @@ class RapidResponsePluginConfig(AppConfig):
                 PluginURLs.RELATIVE_PATH: "urls",
             }
         },
+        PluginSettings.CONFIG: {
+            ProjectType.LMS: {
+                SettingsType.PRODUCTION: {
+                    PluginSettings.RELATIVE_PATH: "settings.production"
+                },
+            }
+        },
         PluginContexts.CONFIG: {
             ProjectType.LMS: {
-                RAPID_RESPONSE_PLUGIN_VIEW_NAME: "ol_openedx_rapid_response_reports.context_api.plugin_context"
+                INSTRUCTOR_DASHBOARD_PLUGIN_VIEW_NAME: "ol_openedx_rapid_response_reports.context_api.plugin_context"
             }
         },
     }
--- src/ol_openedx_rapid_response_reports/context_api.py
+++ src/ol_openedx_rapid_response_reports/context_api.py
@@ -1,14 +1,27 @@
+"""
+ol_openedx_rapid_response_reports Django application plugin context initialization.
+"""
+
 from django.utils.translation import ugettext as _
 from rapid_response_xblock.utils import get_run_data_for_course
+from web_fragments.fragment import Fragment
 
 
 def plugin_context(context):
     """Provide data for the rapid responses dashboard section"""
-    course_key = context["course_key"]
-    return {
+    course_key = context["course"].id
+    sections = context.get("sections", [])
+
+    rapid_response_context = {
         "section_key": "rapid_response",
         "section_display_name": _("Rapid Responses"),
         "problem_runs": get_run_data_for_course(course_key=course_key),
         "course_key": course_key,
         "download_url": "get_rapid_response_report",
+        "fragment": Fragment(),
+        "template_path_prefix": "/",
     }
+    sections.append(rapid_response_context)
+    context["sections"] = sections
+
+    return context
