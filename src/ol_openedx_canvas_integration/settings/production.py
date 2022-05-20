76ef69de89669d1c8a534192c767e68b02e318a4
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2022-05-20 13:18:07 +0500

    feat: serve assets from plugin - nutmeg support (#54)

--- src/ol_openedx_canvas_integration/settings/production.py
+++ src/ol_openedx_canvas_integration/settings/production.py
@@ -1,5 +1,9 @@
 """Production settings unique to canvas integration plugin."""
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 def plugin_settings(settings):
     """Settings for the canvas integration plugin."""
@@ -9,3 +13,8 @@ def plugin_settings(settings):
     settings.CANVAS_BASE_URL = settings.ENV_TOKENS.get(
         "CANVAS_BASE_URL", settings.CANVAS_BASE_URL
     )
+
+    PLUGIN_TEMPLATES_ROOT = os.path.join(BASE_DIR, "templates")
+    for template_engine in settings.TEMPLATES:
+        template_dirs = template_engine["DIRS"]
+        template_dirs.append(PLUGIN_TEMPLATES_ROOT)
