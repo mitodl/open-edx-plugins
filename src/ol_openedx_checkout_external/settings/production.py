d75159d83cfca36127511a64b19e0c8c573e8045
Arslan <arslan.ashraf@arbisoft.com>
2022-05-24 16:33:45 +0500

    fix: load configuration from env_tokens for checkout plugin

--- src/ol_openedx_checkout_external/settings/production.py
+++ src/ol_openedx_checkout_external/settings/production.py
@@ -3,4 +3,6 @@
 
 def plugin_settings(settings):
     """Settings for the external checkout plugin."""
-    settings.MARKETING_SITE_CHECKOUT_URL = None
+    settings.MARKETING_SITE_CHECKOUT_URL = settings.ENV_TOKENS.get(
+        "MARKETING_SITE_CHECKOUT_URL", settings.MARKETING_SITE_CHECKOUT_URL
+    )
