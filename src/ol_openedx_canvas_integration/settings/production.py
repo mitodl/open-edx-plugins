ba15d825a2dba075b6242dcc80e30d197c24d8aa
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2022-06-01 19:08:47 +0500

    build: enhance template config to use Path for templates (#58)
    * build: enhance template config to use Path for templates

--- src/ol_openedx_canvas_integration/settings/production.py
+++ src/ol_openedx_canvas_integration/settings/production.py
@@ -1,8 +1,9 @@
 """Production settings unique to canvas integration plugin."""
 
-import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from path import Path as path
+
+PLUGIN_TEMPLATES_ROOT = path(__file__).abspath().dirname().dirname()
 
 
 def plugin_settings(settings):
@@ -14,7 +15,8 @@ def plugin_settings(settings):
         "CANVAS_BASE_URL", settings.CANVAS_BASE_URL
     )
 
-    PLUGIN_TEMPLATES_ROOT = os.path.join(BASE_DIR, "templates")
+    settings.TEMPLATES = settings.ENV_TOKENS.get("TEMPLATES", settings.TEMPLATES)
+
     for template_engine in settings.TEMPLATES:
         template_dirs = template_engine["DIRS"]
-        template_dirs.append(PLUGIN_TEMPLATES_ROOT)
+        template_dirs.append(PLUGIN_TEMPLATES_ROOT + "/templates")
