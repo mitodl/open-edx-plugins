698b372df03e05df7e016318094992082491cb8d
Shahbaz Shabbir <shbzshbr@gmail.com>
2024-02-02 17:12:56 +0500

    fix: upgrade version to 0.1.1

--- src/ol_openedx_otel_monitoring/__init__.py
+++ src/ol_openedx_otel_monitoring/__init__.py
@@ -2,6 +2,6 @@
 This is an integration of OTel Monitoring
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 default_app_config = "ol_openedx_otel_monitoring.apps.OTelMonitoringConfig"  # pylint: disable=invalid-name
