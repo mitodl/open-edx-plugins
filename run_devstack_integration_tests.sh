a8444d24cc5dba45ce47a5114cef7c586e392217
Muhammad Anas <88967643+Anas12091101@users.noreply.github.com>
2024-02-09 23:10:09 +0500

    feat: added github workflow to run tests for all open-edx plugins on different edx distributions (#277)
    * feat: added ci actions
    
    * feat: added ci actions new
    
    * feat: added ci actions new1
    
    * feat: added ci actions new2
    
    * feat: added ci actions new2
    
    * feat: added ci actions final
    
    * feat: added ci actions final
    
    * feat: added ci actions finalv2
    
    * feat: added ci actions finalv2
    
    * feat: added ci actions finalv2
    
    * feat: added ci actions finalv2
    
    * feat: added ci actions finalv2
    
    * feat: added ci actions finalv2
    
    * feat: added ci actions finalv2
    
    * feat: removed the pylint and pycodestyle
    
    * feat: pytest github step
    
    * feat: looping through plugins and running tests
    
    * feat: looping through plugins and running tests
    
    * feat: looping through plugins and running tests
    
    * feat: enabling tests for different edx distributions
    
    * feat: some cleanup
    
    * feat: added ruff in git workflow
    
    ---------
    
    Co-authored-by: root <root@cms.devstack.edx>

---
+++ run_devstack_integration_tests.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+source /edx/app/edxapp/venvs/edxapp/bin/activate
+
+cd /edx/app/edxapp/edx-platform
+mkdir -p reports
+
+pip install -r ./requirements/edx/testing.txt
+pip install -r ./requirements/edx/paver.txt
+
+mkdir -p test_root  # for edx
+paver update_assets lms --settings=test_static_optimized
+
+cp test_root/staticfiles/lms/webpack-stats.json test_root/staticfiles/webpack-stats.json
+
+
+cd /open-edx-plugins
+
+pip install dist/edx-sysadmin-0.3.0.tar.gz
+pip install dist/ol-openedx-canvas-integration-0.3.0.tar.gz
+pip install dist/ol-openedx-checkout-external-0.1.3.tar.gz
+pip install dist/ol-openedx-course-export-0.1.2.tar.gz
+pip install dist/ol-openedx-course-structure-api-0.1.3.tar.gz
+pip install dist/ol-openedx-git-auto-export-0.3.1.tar.gz
+pip install dist/ol-openedx-logging-0.1.0.tar.gz
+pip install dist/ol-openedx-rapid-response-reports-0.3.0.tar.gz
+pip install dist/ol-openedx-sentry-0.1.2.tar.gz
+
+# Install codecov so we can upload code coverage results
+pip install codecov
+
+# output the packages which are installed for logging
+pip freeze
+
+set +e
+
+
+set +e
+for subdir in "src"/*; do
+    if [ -d "$subdir" ]; then
+        tests_directory="$subdir/tests"
+
+        # Check if tests directory exists
+        if [ -d "$tests_directory" ]; then
+            cp -r /edx/app/edxapp/edx-platform/test_root/ "/open-edx-plugins/$subdir/test_root"
+            echo "==============Running $subdir test==================="
+            cd "$subdir"
+            pytest . --cov .
+            PYTEST_SUCCESS=$?
+
+            if [[ $PYTEST_SUCCESS -ne 0 ]]
+            then
+                echo "pytest exited with a non-zero status"
+                exit $PYTEST_SUCCESS
+            fi
+            coverage xml
+            cd ../..
+        fi
+    fi
+done
+set -e
--- src/edx_sysadmin/tests/test_sysadmin.py
+++ src/edx_sysadmin/tests/test_sysadmin.py
@@ -15,7 +15,7 @@ from django.conf import settings
 from django.test.client import Client
 from django.test.utils import override_settings
 from django.urls import reverse
-from edx_sysadmin.git_import import GitImportErrorNoDir
+from edx_sysadmin.git_import import GitImportNoDirError
 from edx_sysadmin.models import CourseGitLog
 from opaque_keys.edx.locator import CourseLocator
 from openedx.core.djangolib.markup import Text
@@ -114,7 +114,7 @@ class SysadminBaseTestCase(SharedModuleStoreTestCase):  # pragma: allowlist secr
 
 
 @override_settings(
-    GIT_REPO_DIR="/edx-sysadmin" / settings.TEST_ROOT / f"course_repos_{uuid4().hex}",
+    GIT_REPO_DIR=settings.TEST_ROOT / f"course_repos_{uuid4().hex}",
 )
 class TestSysAdminMongoCourseImport(SysadminBaseTestCase):
     """
@@ -153,7 +153,7 @@ class TestSysAdminMongoCourseImport(SysadminBaseTestCase):
         # Create git loaded course
         response = self._add_edx4edx()
         self.assertContains(
-            response, Text(str(GitImportErrorNoDir(settings.GIT_REPO_DIR)))
+            response, Text(str(GitImportNoDirError(settings.GIT_REPO_DIR)))
         )
 
     def test_mongo_course_add_delete(self):
