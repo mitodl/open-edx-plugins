7e29d26ec9a103e2e758cee5007a3609b9b246fd
Muhammad Anas <muhammad.anas@arbisoft.com>
2024-04-18 11:41:35 +0000

    temp: checking something

--- src/rapid_response_xblock/__init__.py
+++ src/rapid_response_xblock/__init__.py
@@ -1,5 +1,5 @@
 # lint-amnesty, pylint: disable=django-not-configured
 """rapid_response_xblock Django app"""
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 default_app_config = "rapid_response_xblock.apps.RapidResponseAppConfig"
