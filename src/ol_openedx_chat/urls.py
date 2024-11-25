4d2002b76bb18bc5500cc0a2cd6d3625a1531164
Muhammad Anas <88967643+Anas12091101@users.noreply.github.com>
2024-11-25 14:02:31 +0500

    fix: failing CI tests (#396)
    * chore: removed python 3.8 from CI
    
    * fix: precommit
    
    * fix: CI tests
    
    * temp: trying without nightly
    
    * revert: temp
    
    * fix: import command problem
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ol_openedx_chat/urls.py
+++ src/ol_openedx_chat/urls.py
@@ -2,7 +2,7 @@
 URLs for ol_openedx_chat.
 """
 
-urlpatterns = [
+urlpatterns = [  # type: ignore[var-annotated]
     # Fill in URL patterns and views here.
     # re_path(r'', TemplateView.as_view(template_name="ol_openedx_chat/base.html")),  # noqa: ERA001, E501
 ]
