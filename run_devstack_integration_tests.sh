b13a9b6287a06ae616f71747f1f564d9610fe645
Muhammad Arslan <arslan.abdulrauf@arbisoft.com>
2025-01-07 14:10:59 +0500

    revert: Revert github action updates

--- run_devstack_integration_tests.sh
+++ run_devstack_integration_tests.sh
@@ -8,23 +8,15 @@ pwd
 mkdir -p reports
 
 pip install -r ./requirements/edx/testing.txt
-if [ "$1" != "master" ]; then
-    pip install -r ./requirements/edx/paver.txt
-fi
+pip install -r ./requirements/edx/paver.txt
 
 # Installing edx-platform
 pip install -e .
 
 mkdir -p test_root  # for edx
+paver update_assets lms --settings=test_static_optimized
 
-if [ "$1" == "master" ]; then
-    npm run build && ./manage.py lms collectstatic --noinput && ./manage.py cms collectstatic --noinput
-    cp /openedx/staticfiles/webpack-stats.json test_root/staticfiles/webpack-stats.json
-else
-    paver update_assets lms --settings=test_static_optimized
-    cp test_root/staticfiles/lms/webpack-stats.json test_root/staticfiles/webpack-stats.json
-fi
-
+cp test_root/staticfiles/lms/webpack-stats.json test_root/staticfiles/webpack-stats.json
 cd /open-edx-plugins
 
 # Installing test dependencies
