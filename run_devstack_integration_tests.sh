a0c97c35b4b6d6a74aff5ffa994c33c368c486b3
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-01-08 08:09:10 +0000

    build: running without checks

--- run_devstack_integration_tests.sh
+++ run_devstack_integration_tests.sh
@@ -8,22 +8,9 @@ pwd
 mkdir -p reports
 
 pip install -r ./requirements/edx/testing.txt
-# if [ "$1" != "master" ]; then
-#     pip install -r ./requirements/edx/paver.txt
-# fi
 
-Installing edx-platform
+# Installing edx-platform
 pip install -e .
-
-# mkdir -p test_root  # for edx
-
-# if [ "$1" == "master" ]; then
-#     mkdir -p /openedx/staticfiles
-#     cp -r /openedx/staticfiles test_root/staticfiles
-# else
-#     paver update_assets lms --settings=test_static_optimized
-#     cp test_root/staticfiles/lms/webpack-stats.json test_root/staticfiles/webpack-stats.json
-# fi
 
 cd /open-edx-plugins
 
