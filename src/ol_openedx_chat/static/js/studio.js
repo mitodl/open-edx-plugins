1641cf1d2ce20576e82930551fa4453c22ba38a8
Asad Ali <asad.ali@arbisoft.com>
2025-03-07 18:48:32 +0500

    feat: enable unit publish when ai chat is enabled/disabled (#449)
    * feat: enable unit publish when ai chat is enabled/disabled
    
    * chore: update build number

--- src/ol_openedx_chat/static/js/studio.js
+++ src/ol_openedx_chat/static/js/studio.js
@@ -2,35 +2,40 @@
     'use strict';
 
     function OpenLearningChatView(runtime, element) {
-        // Sometimes the element is a jQuery object instead of a DOM object which leads to the broken chat form reference
-        if (element instanceof jQuery){
-            element = element[0]
-        }
-        const chatForm = element.querySelector("#ol-chat-form")
-        chatForm.addEventListener("submit", function(event) {
-            event.preventDefault();
-            var studioRuntime = new window.StudioRuntime.v1();
-            const enabledCheck = element.querySelector("#is-enabled-"+chatForm.dataset.blockId);
+        var $element = $(element);
+        var AIChatConfigUpdateInProgress = false;
+        // we need studio runtime to get handler capable of saving xblock data
+        var studioRuntime = new window.StudioRuntime.v1();
 
-            // Get the handler URL
-            const handlerUrl = studioRuntime.handlerUrl(element, 'update_chat_config');
-            var dataToPost = {"is_enabled": enabledCheck.checked};
+        $($element).find('.enabled-check').change(function(e) {
+            var dataToPost = {"is_enabled": this.checked};
+            if (!AIChatConfigUpdateInProgress) {
+                AIChatConfigUpdateInProgress = true;
 
-            $.ajax({
-                url: handlerUrl,
-                method: 'POST',
-                data: JSON.stringify(dataToPost),
-                contentType: 'application/json; charset=utf-8',
-                success: function (response) {
-                    alert("Saved successfully!");
-                },
-                error: function (xhr, status, error) {
-                    alert("There was an error saving the details. Please try again");
-                }
-            });
+                e.preventDefault();
+                runtime.notify('save', {
+                    state: 'start',
+                    element: element,
+                    message: gettext('Updating Chat Config')
+                });
 
+                $.ajax({
+                    type: 'POST',
+                    url: studioRuntime.handlerUrl(element, 'update_chat_config'),
+                    data: JSON.stringify(dataToPost),
+                    dataType: 'json',
+                    contentType: 'application/json; charset=utf-8'
+                }).always(function() {
+                    runtime.notify('save', {
+                        state: 'end',
+                        element: element
+                    });
+                    AIChatConfigUpdateInProgress = false;
+                });
+            }
         });
     }
+
     function initializeOLChat(runtime, element) {
         return new OpenLearningChatView(runtime, element);
     }
