050e83561825793f93b0ce35ae0f5fb9bbdb4074
Asad Ali <asad.ali@arbisoft.com>
2025-03-07 15:24:29 +0500

    chore: remove unused setting

--- src/ol_openedx_chat/settings/common.py
+++ src/ol_openedx_chat/settings/common.py
@@ -8,15 +8,4 @@ def plugin_settings(settings):
     Populate common settings
     """
     env_tokens = getattr(settings, "ENV_TOKENS", {})
-
-    # .. setting_name: OL_CHAT_SETTINGS
-    # .. setting_default: {}
-    # .. setting_description: A dictionary containing the LLM model names as dictionary
-    #   keys and model API tokens/keys as values. This dictionary keys would be
-    #   as LLM model names in the chat settings form in CMS.
-    #
-    #   A sample setting would look like:
-    # .. {"MODEL_NAME1": API_KEY, "MODEL_NAME2": API_KEY}
-
-    settings.OL_CHAT_SETTINGS = env_tokens.get("OL_CHAT_SETTINGS", {})
     settings.LEARN_AI_API_URL = env_tokens.get("LEARN_AI_API_URL", "")
