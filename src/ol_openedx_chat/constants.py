b6ad571f9e8275b54f48a1b8d195a8dc203ef0ca
Asad Ali <asad.ali@arbisoft.com>
2025-03-10 14:49:38 +0500

    feat: add different chat URLs for video and problems

--- src/ol_openedx_chat/constants.py
+++ src/ol_openedx_chat/constants.py
@@ -2,6 +2,10 @@
 # applicable if a block has sub-blocks or sub category, that should be added in the list
 VIDEO_BLOCK_CATEGORY = "video"
 PROBLEM_BLOCK_CATEGORY = "problem"
+LEARN_AI_CHAT_URL_PATH = {
+    VIDEO_BLOCK_CATEGORY: "http/video_gpt_agent/",
+    PROBLEM_BLOCK_CATEGORY: "http/tutor_agent/",
+}
 CHAT_APPLICABLE_BLOCKS = [PROBLEM_BLOCK_CATEGORY, VIDEO_BLOCK_CATEGORY]
 
 ENGLISH_LANGUAGE_TRANSCRIPT = "en"
