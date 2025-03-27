c93b1a07f83e7674b6082a403806ea813357ae58
asajjad2 <areebsajjad21@gmail.com>
2025-03-28 03:57:12 +0500

    fix: ruff errors

--- src/ol_openedx_course_export/signals.py
+++ src/ol_openedx_course_export/signals.py
@@ -40,9 +40,9 @@ def proto2_to_proto1(body, headers):
     return new_body
 
 
-def _extract_proto2_headers(id, retries, eta, expires, group, timelimit, task, **_):
+def _extract_proto2_headers(task_id, retries, eta, expires, group, timelimit, task, **_):  # noqa: PLR0913
     return {
-        "id": id,
+        "id": task_id,
         "task": task,
         "retries": retries,
         "eta": eta,
