41901d72111182d697f05b0bbf5ac8941780b14d
Asad Ali <asad.ali@arbisoft.com>
2025-04-04 18:13:49 +0500

    add course key deprecated test

--- src/ol_openedx_chat/tests/test_utils.py
+++ src/ol_openedx_chat/tests/test_utils.py
@@ -8,29 +8,33 @@ from ol_openedx_chat.utils import (
     is_aside_applicable_to_block,
     is_ol_chat_enabled_for_course,
 )
+from opaque_keys.edx.locator import CourseLocator
 
 
 @ddt
 class OLChatUtilTests(OLChatTestCase):
     @data(
         *[
-            ("problem", True, True, True),
-            ("problem", True, False, False),
-            ("problem", False, True, True),
-            ("problem", False, False, False),
-            ("video", True, True, True),
-            ("video", True, False, True),
-            ("video", False, True, False),
-            ("video", False, False, False),
+            ("problem", True, True, True, True),
+            ("problem", True, True, True, False),
+            ("problem", True, False, False, False),
+            ("problem", False, True, True, False),
+            ("problem", False, False, False, False),
+            ("video", True, True, True, True),
+            ("video", True, True, True, False),
+            ("video", True, False, True, False),
+            ("video", False, True, False, False),
+            ("video", False, False, False, False),
         ]
     )
     @unpack
-    def test_is_ol_chat_enabled_for_course(
+    def test_is_ol_chat_enabled_for_course(  # noqa: PLR0913
         self,
         block_category,
         video_block_setting,
         problem_block_setting,
         expected_is_enabled,
+        is_course_key_deprecated,
     ):
         """
         Test the is_ol_chat_enabled_for_course function
@@ -46,6 +50,13 @@ class OLChatUtilTests(OLChatTestCase):
             block = (
                 self.problem_block if block_category == "problem" else self.video_block
             )
+            if is_course_key_deprecated:
+                block.usage_key.course_key.deprecated = True
+                block.usage_key.course_key = CourseLocator(
+                    block.usage_key.course_key.org,
+                    block.usage_key.course_key.course,
+                    block.usage_key.course_key.run,
+                )
             assert is_ol_chat_enabled_for_course(block) == expected_is_enabled
 
     @data(
