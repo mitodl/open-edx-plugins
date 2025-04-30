cdabc1157c356aa896ee3de63797b3a483014031
Asad Ali <asad.ali@arbisoft.com>
2025-04-30 18:17:11 +0500

    feat: add ol-openedx-course-propagator

---
+++ src/ol_openedx_course_propagator/apps.py
@@ -0,0 +1,10 @@
+"""
+App configuration for ol-openedx-course-propagator plugin
+"""
+
+from django.apps import AppConfig
+
+
+class OLOpenEdxCoursePropagatorConfig(AppConfig):
+    name = "ol_openedx_course_propagator"
+    verbose_name = "Open edX Course Propagator"
---
+++ src/ol_openedx_course_propagator/utils.py
@@ -0,0 +1,30 @@
+"""
+Utility methods for ol-openedx-course-propagator plugin
+"""
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.django import SignalHandler
+from opaque_keys.edx.locator import CourseLocator
+
+def copy_course(user_id, source_course_id, dest_course_id):
+    """
+    Copy course content from source course to destination course.
+    """
+    module_store = modulestore()
+    source_course_key = CourseLocator.from_string(source_course_id)
+    source_course_draft = source_course_key.for_branch("draft-branch")
+    source_course_published = source_course_key.for_branch("published-branch")
+    subtree_list = [module_store.make_course_usage_key(source_course_key)]
+
+    dest_course_key = CourseLocator.from_string(dest_course_id)
+    dest_course_draft = dest_course_key.for_branch("draft-branch")
+    dest_course_published = dest_course_key.for_branch("published-branch")
+
+    source_modulestore = module_store._get_modulestore_for_courselike(source_course_key)
+    # for a temporary period of time, we may want to hardcode dest_modulestore as split if there's a split
+    # to have only course re-runs go to split. This code, however, uses the config'd priority
+    dest_modulestore = module_store._get_modulestore_for_courselike(dest_course_key)
+    if source_modulestore == dest_modulestore:
+        source_modulestore.copy(user_id, source_course_draft, dest_course_draft, subtree_list)
+        source_modulestore.copy(user_id, source_course_published, dest_course_published, subtree_list)
+        SignalHandler.course_published.send(sender=None, course_key=dest_course_key)
+        return
