fdab2cd3d44491796d2aae81fae7226b31d4a0a4
Asad Ali <asad.ali@arbisoft.com>
2025-06-23 13:15:19 +0500

    update settings

--- src/ol_openedx_course_sync/settings/common.py
+++ src/ol_openedx_course_sync/settings/common.py
@@ -3,7 +3,4 @@
 
 def plugin_settings(settings):
     """Settings for the course sync plugin."""  # noqa: D401
-    env_tokens = getattr(settings, "ENV_TOKENS", {})
-    settings.OL_OPENEDX_COURSE_SYNC_SERVICE_WORKER_USERNAME = env_tokens.get(
-        "OL_OPENEDX_COURSE_SYNC_SERVICE_WORKER_USERNAME", ""
-    )
+    settings.OL_OPENEDX_COURSE_SYNC_SERVICE_WORKER_USERNAME = ""
