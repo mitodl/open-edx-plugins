8d464d7f026d436b90c212d1d01f0772eee6b27e
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2025-07-04 18:42:41 +0500

    fix: order of the auto export calls (#515)
    * fix: order of the auto export calls
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ol_openedx_git_auto_export/signals.py
+++ src/ol_openedx_git_auto_export/signals.py
@@ -2,8 +2,7 @@ import logging
 import os
 
 from django.conf import settings
-from django.dispatch import receiver
-from xmodule.modulestore.django import SignalHandler, modulestore
+from xmodule.modulestore.django import modulestore
 
 from ol_openedx_git_auto_export.constants import ENABLE_GIT_AUTO_EXPORT
 from ol_openedx_git_auto_export.tasks import async_export_to_git
@@ -11,7 +10,6 @@ from ol_openedx_git_auto_export.tasks import async_export_to_git
 log = logging.getLogger(__name__)
 
 
-@receiver(SignalHandler.course_published)
 def listen_for_course_publish(
     sender,  # noqa: ARG001
     course_key,
@@ -20,16 +18,6 @@ def listen_for_course_publish(
     """
     Receives publishing signal and performs publishing related workflows
     """
-    git_repo_export_dir = getattr(
-        settings, "GIT_REPO_EXPORT_DIR", "/openedx/export_course_repos"
-    )
-    if not os.path.exists(git_repo_export_dir):  # noqa: PTH110
-        # for development/docker/vagrant if GIT_REPO_EXPORT_DIR folder does not exist then create it  # noqa: E501
-        log.error(
-            "GIT_REPO_EXPORT_DIR is not available in settings, please create it first"
-        )
-        os.makedirs(git_repo_export_dir, 0o755)  # noqa: PTH103
-
     if settings.FEATURES.get("ENABLE_EXPORT_GIT") and settings.FEATURES.get(
         ENABLE_GIT_AUTO_EXPORT
     ):
@@ -40,9 +28,21 @@ def listen_for_course_publish(
                 course_module.id,
             )
             return
-        # If the Git auto-export is enabled, push the course changes to Git
+
+        git_repo_export_dir = getattr(
+            settings, "GIT_REPO_EXPORT_DIR", "/openedx/export_course_repos"
+        )
+        if not os.path.exists(git_repo_export_dir):  # noqa: PTH110
+            # for development/docker/vagrant if GIT_REPO_EXPORT_DIR folder does not exist then create it  # noqa: E501
+            log.error(
+                "GIT_REPO_EXPORT_DIR is not available in settings, please create it first"  # noqa: E501
+            )
+            os.makedirs(git_repo_export_dir, 0o755)  # noqa: PTH103
+
         log.info(
             "Course published with auto-export enabled. Starting export... (course id: %s)",  # noqa: E501
             course_key,
         )
+
+        # If the Git auto-export is enabled, push the course changes to Git
         async_export_to_git.delay(str(course_key))
