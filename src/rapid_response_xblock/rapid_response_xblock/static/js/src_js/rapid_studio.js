c441067a0bfeef63aabe1aab4e819367f6154cbf
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2025-07-07 14:37:42 +0500

    chore: refactor rapid response to use studio runtime

--- src/rapid_response_xblock/rapid_response_xblock/static/js/src_js/rapid_studio.js
+++ src/rapid_response_xblock/rapid_response_xblock/static/js/src_js/rapid_studio.js
@@ -2,30 +2,44 @@
   'use strict';
 
   function RapidResponseAsideStudioView(runtime, element) {
-    var toggleEnabledUrl = runtime.handlerUrl(element, '');
-    // Redirect this call to our own API instead of xBlock handler because the handler callbacks are not supported
-    // for the studio preview. The calls can have two formats 1) "preview/xblock" when called from studio preview
-    // 2) "/xblock" when called from Studio plugin(Tab) settings.
-    toggleEnabledUrl = toggleEnabledUrl.replace("preview/xblock", "toggle-rapid-response")
-    toggleEnabledUrl = toggleEnabledUrl.replace("/xblock", "/toggle-rapid-response")
+    // we need studio runtime to get handler capable of saving xblock data in Studio
+    var studioRuntime = new window.StudioRuntime.v1();
+
+    var toggleEnabledUrl = studioRuntime.handlerUrl(element, 'toggle_block_enabled');
     var $element = $(element);
 
     var rapidTopLevelSel = '.rapid-response-block';
     var rapidBlockContentSel = '.rapid-response-content';
     var enabledCheckboxSel = '.rapid-enabled-toggle';
     var toggleTemplate = _.template($(element).find("#rapid-response-toggle-tmpl").text());
-
+    var rapidResponseToggleInProgress = false;
     function render(state) {
       // Render template
       var $rapidBlockContent = $element.find(rapidBlockContentSel);
       $rapidBlockContent.html(toggleTemplate(state));
 
-      $rapidBlockContent.find(enabledCheckboxSel).click(function() {
-        $.post(toggleEnabledUrl).then(
-          function(state) {
-            render(state);
-          }
-        );
+      $rapidBlockContent.find(enabledCheckboxSel).click(function(e) {
+        if (!rapidResponseToggleInProgress) {
+          rapidResponseToggleInProgress = true;
+          e.preventDefault();
+          runtime.notify('save', {
+              state: 'start',
+              element: element,
+              message: gettext('Toggling rapid response')
+          });
+
+          $.post(toggleEnabledUrl).then(
+            function(state) {
+              render(state);
+            }
+          ) .always(function() {
+            runtime.notify('save', {
+              state: 'end',
+              element: element
+            });
+          });
+          rapidResponseToggleInProgress = false;
+        }
       });
     }
 
