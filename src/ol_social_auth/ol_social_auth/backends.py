eb2d015e9fba8679f3d1e3cef7bf98470e0632d8
Asad Ali <asad.ali@arbisoft.com>
2025-09-02 18:14:57 +0500

    feat: update olauth2 to use OIDC based discovery (#596)
    * feat: update olauth2 to use OIDC based discovery
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * test: fix tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * style: fmt
    
    * fix tests
    
    * update version + uv.lock
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ol_social_auth/ol_social_auth/backends.py
+++ src/ol_social_auth/ol_social_auth/backends.py
@@ -1,5 +1,6 @@
 """Open Learning social auth backend"""
 
+import requests
 from social_core.backends.oauth import BaseOAuth2
 
 
@@ -16,13 +17,32 @@ class OLOAuth2(BaseOAuth2):
     # at a minimum we need to be able to read the user
     DEFAULT_SCOPE = ["user:read"]
 
+    _discovery_doc = None  # cache to avoid multiple network calls
+
+    def _get_metadata(self):
+        """Fetch OpenID provider metadata"""
+        # For xPro, we don't have a discovery URL, so we get the URLs from settings
+        if not self.setting("DISCOVERY_URL"):
+            return {
+                "authorization_endpoint": self.setting("AUTHORIZATION_URL"),
+                "token_endpoint": self.setting("ACCESS_TOKEN_URL"),
+                "userinfo_endpoint": self.api_url("api/users/me"),
+            }
+
+        if not self._discovery_doc:
+            discovery_url = self.setting("DISCOVERY_URL")
+            resp = requests.get(discovery_url, timeout=5)
+            resp.raise_for_status()
+            self._discovery_doc = resp.json()
+        return self._discovery_doc
+
     def authorization_url(self):
         """Provides authorization_url from settings"""  # noqa: D401
-        return self.setting("AUTHORIZATION_URL")
+        return self._get_metadata()["authorization_endpoint"]
 
     def access_token_url(self):
         """Provides access_token_url from settings"""  # noqa: D401
-        return self.setting("ACCESS_TOKEN_URL")
+        return self._get_metadata()["token_endpoint"]
 
     def api_root(self):
         """Returns the API root as configured"""  # noqa: D401
@@ -58,6 +78,6 @@ class OLOAuth2(BaseOAuth2):
 
     def user_data(self, access_token, *args, **kwargs):  # noqa: ARG002
         """Loads user data from MIT application"""  # noqa: D401
-        url = self.api_url("api/users/me")
+        url = self._get_metadata()["userinfo_endpoint"]
         headers = {"Authorization": f"Bearer {access_token}"}
         return self.get_json(url, headers=headers)
