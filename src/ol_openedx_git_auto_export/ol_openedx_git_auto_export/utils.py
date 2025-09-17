54c0e1b2988a8f00bcc9dd61af1f94bb318ce6ad
Asad Ali <asad.ali@arbisoft.com>
2025-09-17 13:09:09 +0500

    fmt

--- src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/utils.py
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/utils.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 def get_publisher_username(course_module):
     """
-    Returns the username of the user who published the course.
+    Return the username of the user who published the course.
     If the user cannot be found, returns None.
     """
     if not course_module:
