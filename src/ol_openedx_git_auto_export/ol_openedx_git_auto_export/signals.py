ee16461c0d39db80376c7107ad49d1c9da76d3ed
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2025-09-19 00:40:35 +0500

    feat: create github repo on course creation

--- src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/signals.py
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/signals.py
@@ -1,12 +1,21 @@
 import logging
-import os
 
+import requests
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.template.defaultfilters import slugify
 from xmodule.modulestore.django import modulestore
 
-from ol_openedx_git_auto_export.constants import ENABLE_GIT_AUTO_EXPORT
+from ol_openedx_git_auto_export.constants import (
+    ENABLE_AUTO_GITHUB_REPO_CREATION,
+    ENABLE_GIT_AUTO_EXPORT,
+)
+from ol_openedx_git_auto_export.models import CourseGitRepo
 from ol_openedx_git_auto_export.tasks import async_export_to_git
-from ol_openedx_git_auto_export.utils import get_publisher_username
+from ol_openedx_git_auto_export.utils import (
+    get_or_create_git_export_repo_dir,
+    get_publisher_username,
+)
 
 log = logging.getLogger(__name__)
 
@@ -19,27 +28,12 @@ def listen_for_course_publish(
     """
     Receives publishing signal and performs publishing related workflows
     """
+
     if settings.FEATURES.get("ENABLE_EXPORT_GIT") and settings.FEATURES.get(
         ENABLE_GIT_AUTO_EXPORT
     ):
         course_module = modulestore().get_course(course_key)
-        if not course_module.giturl:
-            log.info(
-                "Course %s does not have a GIT URL set in course advance settings, skipping export.",  # noqa: E501
-                course_module.id,
-            )
-            return
-
-        git_repo_export_dir = getattr(
-            settings, "GIT_REPO_EXPORT_DIR", "/openedx/export_course_repos"
-        )
-        if not os.path.exists(git_repo_export_dir):  # noqa: PTH110
-            # for development/docker/vagrant if GIT_REPO_EXPORT_DIR folder does not exist then create it  # noqa: E501
-            log.error(
-                "GIT_REPO_EXPORT_DIR is not available in settings, please create it first"  # noqa: E501
-            )
-            os.makedirs(git_repo_export_dir, 0o755)  # noqa: PTH103
-
+        get_or_create_git_export_repo_dir()
         log.info(
             "Course published with auto-export enabled. Starting export... (course id: %s)",  # noqa: E501
             course_key,
@@ -48,3 +42,71 @@ def listen_for_course_publish(
         user = get_publisher_username(course_module)
         # If the Git auto-export is enabled, push the course changes to Git
         async_export_to_git.delay(str(course_key), user)
+
+
+def listen_for_course_created(**kwargs):
+    course_id = kwargs.get("course").course_key
+    course_id_slugified = slugify(str(course_id))
+    if not settings.FEATURES.get(ENABLE_AUTO_GITHUB_REPO_CREATION):
+        log.info(
+            "GitHub repo creation is disabled. Skipping GitHub repo creation for course %s",  # noqa: E501
+            course_id,
+        )
+        return None
+
+    # SignalHandler.course_published is called before COURSE_CREATED signal
+    if CourseGitRepo.objects.filter(course_id=str(course_id)).exists():
+        log.info(
+            "GitHub repository already exists for course %s. Skipping creation.",
+            course_id,
+        )
+        return None
+
+    gh_access_token = settings.GITHUB_ACCESS_TOKEN
+    if not settings.GITHUB_ORG_API_URL or not gh_access_token:
+        error_msg = "GITHUB_ORG_API_URL or GITHUB_ACCESS_TOKEN is not set in settings. Skipping GitHub repo creation."  # noqa: E501
+        raise ImproperlyConfigured(error_msg)
+
+    url = f"{settings.GITHUB_ORG_API_URL}/repos"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {gh_access_token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    payload = {
+        "name": course_id_slugified,
+        "description": f"Git repository for {course_id!s}",
+        "private": True,
+        "has_issues": False,
+        "has_project": False,
+        "has_wiki": False,
+        "auto_init": True,
+    }
+    response = requests.post(url, headers=headers, json=payload, timeout=30)
+    if response.status_code != 201:  # noqa: PLR2004
+        log.error(
+            "Failed to create GitHub repository for course %s: %s",
+            course_id,
+            response.json(),
+        )
+        return None
+
+    repo_data = response.json()
+    ssh_url = repo_data.get("ssh_url")
+    if ssh_url:
+        CourseGitRepo.objects.create(
+            course_id=str(course_id),
+            git_url=ssh_url,
+        )
+        log.info(
+            "GitHub repository created for course %s: %s",
+            course_id,
+            ssh_url,
+        )
+    else:
+        log.error(
+            "Failed to retrieve SSH URL for GitHub repository for course %s",
+            course_id,
+        )
+
+    return ssh_url
--- src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/utils.py
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/utils.py
@@ -2,7 +2,13 @@
 Utility functions for the ol_openedx_git_auto_export app.
 """
 
+import logging
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import User
+
+log = logging.getLogger(__name__)
 
 
 def get_publisher_username(course_module):
@@ -19,3 +25,17 @@ def get_publisher_username(course_module):
 
     user = User.objects.filter(id=user_id).first()
     return user.username if user else None
+
+
+def get_or_create_git_export_repo_dir():
+    git_repo_export_dir = getattr(
+        settings, "GIT_REPO_EXPORT_DIR", "/openedx/export_course_repos"
+    )
+    if not os.path.exists(git_repo_export_dir):  # noqa: PTH110
+        # for development/docker/vagrant if GIT_REPO_EXPORT_DIR folder does not exist then create it  # noqa: E501
+        log.error(
+            "GIT_REPO_EXPORT_DIR is not available in settings, please create it first"
+        )
+        os.makedirs(git_repo_export_dir, 0o755)  # noqa: PTH103
+
+    return git_repo_export_dir
