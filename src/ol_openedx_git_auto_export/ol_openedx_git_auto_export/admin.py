9d7b274974ef6f952b550242024056714e045b3b
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2025-09-19 00:40:35 +0500

    feat: create github repo on course creation

---
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/admin.py
@@ -0,0 +1,22 @@
+"""
+Django admin pages for git-auto-export plugin
+"""
+
+from django.contrib import admin
+
+from ol_openedx_git_auto_export.models import CourseGitRepo
+
+
+class CourseGitRepoAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the CourseGitRepo model.
+    """
+
+    list_display = ("course_id", "git_url")
+    search_fields = ("course_id", "git_url")
+    list_filter = ("course_id",)
+    ordering = ("-created_at",)
+    list_per_page = 20
+
+
+admin.site.register(CourseGitRepo, CourseGitRepoAdmin)
---
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/management/commands/migrate_giturl.py
@@ -0,0 +1,54 @@
+from django.core.management.base import BaseCommand
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from openedx_events.content_authoring.data import CourseData
+from xmodule.modulestore.django import modulestore
+
+from ol_openedx_git_auto_export.models import CourseGitRepo
+from ol_openedx_git_auto_export.signals import listen_for_course_created
+
+
+class Command(BaseCommand):
+    help = "Migrate git URLs from courses to CourseGitRepo model"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "course_ids",
+            nargs="*",
+            type=str,
+            help="List of course IDs to migrate their giturl",
+        )
+
+    def handle(self, *args, **options):  # noqa: ARG002
+        course_ids = options.get("course_ids")
+        courses = CourseOverview.objects.all()
+        if course_ids:
+            courses = courses.filter(id__in=course_ids)
+        return
+        seen_giturls = set()
+        for course in courses:
+            course_module = modulestore().get_course(course.id, depth=1)
+            giturl = course_module.giturl
+            if giturl and giturl not in seen_giturls:
+                seen_giturls.add(giturl)
+                self.stdout.write(
+                    self.style.SUCCESS(f"Course {course.id} has giturl: {giturl}")
+                )
+                CourseGitRepo.objects.get_or_create(
+                    course_id=course.id,
+                    defaults={"git_url": giturl},
+                )
+            elif giturl and giturl in seen_giturls:
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"Course {course.id} has a duplicate giturl: {giturl}"
+                    )
+                )
+                ssh_url = listen_for_course_created(CourseData(course_key=course.id))
+                if ssh_url:
+                    seen_giturls.add(ssh_url)
+            else:
+                self.stdout.write(
+                    self.style.WARNING(f"Course {course.id} does not have a giturl.")
+                )
+
+        self.stdout.write(self.style.SUCCESS("Git URLs migrated successfully."))
