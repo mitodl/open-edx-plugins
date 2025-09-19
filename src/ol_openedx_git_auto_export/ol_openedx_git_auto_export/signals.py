8c70cc38f8f571fca0a64c1e6ef9ea3ec3385f55
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2025-09-19 16:02:55 +0500

    fix: re-run github repo creation added

--- src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/signals.py
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/signals.py
@@ -4,11 +4,14 @@ import requests
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.template.defaultfilters import slugify
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from openedx_events.content_authoring.data import CourseData
 from xmodule.modulestore.django import modulestore
 
 from ol_openedx_git_auto_export.constants import (
     ENABLE_AUTO_GITHUB_REPO_CREATION,
     ENABLE_GIT_AUTO_EXPORT,
+    RE_RUN_DELAY_TIME,
 )
 from ol_openedx_git_auto_export.models import CourseGitRepo
 from ol_openedx_git_auto_export.tasks import async_export_to_git
@@ -38,6 +41,20 @@ def listen_for_course_publish(
             "Course published with auto-export enabled. Starting export... (course id: %s)",  # noqa: E501
             course_key,
         )
+        # HACK: To create auto git repo for Re-runs as it does not emit COURSE_CREATED signal  # noqa: E501 FIX004
+        # if course_overview.created and course_module.published_on has difference of less than 2 minutes  # noqa: E501
+        # Consider creating Giturl for the course if it doesn't exist
+        course_overview = CourseOverview.get_from_id(course_key)
+        time_difference = course_module.published_on - course_overview.created
+        if (
+            time_difference.total_seconds() < RE_RUN_DELAY_TIME
+            and not CourseGitRepo.objects.filter(course_id=str(course_key)).exists()
+        ):
+            log.info(
+                "Creating GitHub repository for course (Re-run) %s",
+                course_key,
+            )
+            listen_for_course_created(course=CourseData(course_key=course_key))
 
         user = get_publisher_username(course_module)
         # If the Git auto-export is enabled, push the course changes to Git
