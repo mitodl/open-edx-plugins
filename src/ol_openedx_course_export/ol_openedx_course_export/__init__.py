f4e714feca53b0131f88d0e5b20cafaeca08cdaa
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2025-09-29 17:47:22 +0500

    additional changes, and fixes

--- src/ol_openedx_course_export/ol_openedx_course_export/__init__.py
+++ src/ol_openedx_course_export/ol_openedx_course_export/__init__.py
@@ -1,7 +1,3 @@
 """
 This is an integration of course export API.
 """
-
-__version__ = "0.1.0"
-
-default_app_config = "ol_openedx_course_export.app.CourseExportConfig"
--- src/ol_openedx_rapid_response_reports/ol_openedx_rapid_response_reports/__init__.py
+++ src/ol_openedx_rapid_response_reports/ol_openedx_rapid_response_reports/__init__.py
@@ -1,7 +1,1 @@
-"""
-A plugin to add support for rapid response reports in Instructor Dashboard.
-"""
-
-__version__ = "0.1.0"
-
-default_app_config = "ol_openedx_rapid_response_reports.app.RapidResponsePluginConfig"  # pylint: disable=invalid-name
+"""A plugin to add support for rapid response reports in Instructor Dashboard."""
