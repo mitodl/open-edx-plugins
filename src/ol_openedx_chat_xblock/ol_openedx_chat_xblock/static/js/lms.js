dd773ec9a99ecfccf93843b56a2be536d699868e
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2025-10-14 15:42:18 +0500

    feat: add feedback API integration (#627)
    * feat: add feedback API integration
    
    * bump version
    
    * feedback
    
    * fix tests

--- src/ol_openedx_chat_xblock/ol_openedx_chat_xblock/static/js/lms.js
+++ src/ol_openedx_chat_xblock/ol_openedx_chat_xblock/static/js/lms.js
@@ -1,7 +1,8 @@
 function OLChatBlock(runtime, element, init_args) {
-    import("https://cdn.jsdelivr.net/npm/@mitodl/smoot-design@6.17.3/dist/bundles/aiChat.es.js").then(aiChat => {
+    import("https://cdn.jsdelivr.net/npm/@mitodl/smoot-design@6.18.2/dist/bundles/aiChat.es.js").then(aiChat => {
         const requestOpts = {
             apiUrl: runtime.handlerUrl(element, 'ol_chat'),
+            feedbackApiUrl: init_args.chat_rating_url,
             transformBody: (messages, { problem_set_title }) => {
                 return {
                     message: messages[messages.length - 1].content,
@@ -29,7 +30,7 @@ function OLChatBlock(runtime, element, init_args) {
                         role: "assistant",
                         content: init_args.problem_set_initial_message,
                     },
-                ],
+                ]
             },
             {
                 container: document.getElementById(`ai-chat-container-${init_args.block_id}`),
