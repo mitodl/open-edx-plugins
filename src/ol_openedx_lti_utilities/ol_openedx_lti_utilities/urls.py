860ae92ec6b734c5456b187e9d6d1e07faa25a83
Nathan Levesque <rhysyngsun@users.noreply.github.com>
2025-12-03 12:54:14 -0500

    Condition lti_utilies urls.py on LTI feature (#665)
    * Condition lti_utilies urls.py on LTI feature
    
    * Bump LTI utilities version
    
    ---------
    
    Co-authored-by: Tobias Macey <tmacey@mit.edu>

--- src/ol_openedx_lti_utilities/ol_openedx_lti_utilities/urls.py
+++ src/ol_openedx_lti_utilities/ol_openedx_lti_utilities/urls.py
@@ -2,14 +2,16 @@
 OL Open edX LTI Utilities URLs
 """
 
+from django.conf import settings
 from django.urls import re_path
 
-from ol_openedx_lti_utilities.views import LtiUserFixView
+if settings.FEATURES.get("ENABLE_LTI_PROVIDER"):
+    from ol_openedx_lti_utilities.views import LtiUserFixView
 
-urlpatterns = [
-    re_path(
-        r"^",
-        LtiUserFixView.as_view(),
-        name="lti_user_fix",
-    ),
-]
+    urlpatterns = [
+        re_path(
+            r"^",
+            LtiUserFixView.as_view(),
+            name="lti_user_fix",
+        ),
+    ]
