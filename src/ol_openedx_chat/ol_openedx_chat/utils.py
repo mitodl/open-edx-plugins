6e9b67e4d425050321f4aaab86653e7dc28e4de5
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2025-12-08 20:28:04 +0500

    feat: add thread_id and checkpoint_pk in aside events (#664)
    * feat: add thread_id and checkpoint_pk in aside events

--- src/ol_openedx_chat/ol_openedx_chat/utils.py
+++ src/ol_openedx_chat/ol_openedx_chat/utils.py
@@ -1,10 +1,16 @@
 # ruff: noqa: E501
 """Utility methods for the AI chat"""
 
+import json
+import logging
+import re
+
 from lms.djangoapps.courseware.courses import get_course_by_id
 from opaque_keys.edx.locator import CourseLocator
 
 from ol_openedx_chat.constants import BLOCK_TYPE_TO_SETTINGS, CHAT_APPLICABLE_BLOCKS
+
+log = logging.getLogger(__name__)
 
 
 def is_aside_applicable_to_block(block):
@@ -40,3 +46,37 @@ def is_ol_chat_enabled_for_course(block):
     other_course_settings = course.other_course_settings
     block_type = getattr(block, "category", None)
     return other_course_settings.get(BLOCK_TYPE_TO_SETTINGS.get(block_type))
+
+
+def get_checkpoint_and_thread_id(content):
+    """
+    Extract the checkpoint ID and thread ID from chat response content.
+
+    Args:
+        content (str): The content from the chat response.
+    Returns:
+        tuple: A tuple containing the thread ID and checkpoint PK, or (None, None)
+                if extraction fails.
+    """
+    if content is None:
+        return None, None
+    match = None
+    try:
+        # Decode bytes to string
+        content_str = content.decode("utf-8") if isinstance(content, bytes) else content
+        # The content from the chat response contains values with JSON data.
+        # e.g. It looks like '_content': b'Hello! How can I help you?\n\n
+        # <!-- {"checkpoint_pk": 123, "thread_id": "abc123"} -->\n\n'
+        match = re.search(r"<!-- ({.*}) -->", content_str)
+        if match:
+            dict_values = json.loads(match.group(1))
+            return dict_values.get("thread_id", None), str(
+                dict_values.get("checkpoint_pk", None)
+            )
+    except Exception:
+        log.exception(
+            "Couldn't parse content/suffix to get Thread_id and Checkpoint_pk."
+            " content: %s",
+            content,
+        )
+    return None, None
