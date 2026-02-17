cd876312fa0b504d194924bf96c63a8a16bc2d54
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2026-02-17 20:51:33 +0500

    config: add plugin settings for the course sync plugin (#735)

--- src/ol_openedx_course_sync/ol_openedx_course_sync/apps.py
+++ src/ol_openedx_course_sync/ol_openedx_course_sync/apps.py
@@ -3,8 +3,8 @@ App configuration for ol-openedx-course-sync plugin
 """
 
 from django.apps import AppConfig
-from edx_django_utils.plugins import PluginSignals
-from openedx.core.djangoapps.plugins.constants import ProjectType
+from edx_django_utils.plugins import PluginSettings, PluginSignals
+from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
 
 class OLOpenEdxCourseSyncConfig(AppConfig):
@@ -25,6 +25,14 @@ class OLOpenEdxCourseSyncConfig(AppConfig):
                         PluginSignals.DISPATCH_UID: "ol_openedx_course_sync.signals.listen_for_course_publish",  # noqa: E501
                     }
                 ],
+            },
+        },
+        PluginSettings.CONFIG: {
+            ProjectType.CMS: {
+                SettingsType.PRODUCTION: {
+                    PluginSettings.RELATIVE_PATH: "settings.production"
+                },
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: "settings.common"},
             },
         },
     }
