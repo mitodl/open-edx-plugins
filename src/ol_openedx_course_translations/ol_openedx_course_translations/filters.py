fb94e611ce1d81c498da40455d09b96dac4dd25b
Asad Ali <asad.ali@arbisoft.com>
2026-02-19 23:45:22 +0500

    fix: filter fixed for standalone xblock render /xblock/{block_key}

--- src/ol_openedx_course_translations/ol_openedx_course_translations/filters.py
+++ src/ol_openedx_course_translations/ol_openedx_course_translations/filters.py
@@ -23,21 +23,36 @@ class AddDestLangForVideoBlock(PipelineStep):
         Add the destination language to the student view context if a video block
         with transcripts in the course language is found among the child blocks.
         """
-        for child in dict(context)["block"].children:
-            if child.block_type == VIDEO_BLOCK_TYPE:
-                student_view_context["dest_lang"] = (
-                    ENGLISH_LANGUAGE_CODE  # default to English
-                )
-                video_block = modulestore().get_item(child)
-                transcripts_info = video_block.get_transcripts_info()
-                dest_lang = getattr(
-                    context.get("course", None), "language", ENGLISH_LANGUAGE_CODE
-                )
-                dest_lang = LanguageCode(dest_lang).to_bcp47()
-                if (
-                    transcripts_info
-                    and transcripts_info.get("transcripts", {})
-                    and dest_lang in transcripts_info["transcripts"]
-                ):
-                    student_view_context["dest_lang"] = dest_lang
+
+        def set_dest_lang_for_video_block(block_key):
+            """
+            Set the destination language for video blocks with
+            transcripts in the course language.
+            """
+            student_view_context["dest_lang"] = (
+                ENGLISH_LANGUAGE_CODE  # default to English
+            )
+            video_block = modulestore().get_item(block_key)
+            transcripts_info = video_block.get_transcripts_info()
+            dest_lang = getattr(
+                context.get("course", None), "language", ENGLISH_LANGUAGE_CODE
+            )
+            dest_lang = LanguageCode(dest_lang).to_bcp47()
+            if (
+                transcripts_info
+                and transcripts_info.get("transcripts", {})
+                and dest_lang in transcripts_info["transcripts"]
+            ):
+                student_view_context["dest_lang"] = dest_lang
+
+        block = dict(context)["block"]
+        block_usage_key = block.usage_key
+        block_type = block_usage_key.block_type
+        if block_type == "vertical":
+            for child in getattr(block, "children", []):
+                if child.block_type == VIDEO_BLOCK_TYPE:
+                    set_dest_lang_for_video_block(child)
+        elif block_type == VIDEO_BLOCK_TYPE:
+            set_dest_lang_for_video_block(block_usage_key)
+
         return {"context": context, "student_view_context": student_view_context}
