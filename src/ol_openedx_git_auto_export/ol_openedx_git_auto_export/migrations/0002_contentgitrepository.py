a378c05055635936ea1f5ca8316c74ff6a9d539f
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2026-02-20 17:37:21 +0500

    fix: migrations fixed from data-migration to renaming model and field

--- src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/migrations/0002_contentgitrepository.py
+++ src/ol_openedx_git_auto_export/ol_openedx_git_auto_export/migrations/0002_contentgitrepository.py
@@ -1,49 +1,16 @@
-# Generated migration for ContentGitRepository model
+# Generated migration for renaming CourseGitRepository to ContentGitRepository
 
-import django.utils.timezone
-import model_utils.fields
-import opaque_keys.edx.django.models
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
     dependencies = [
         ("ol_openedx_git_auto_export", "0001_initial"),
-    ]  # type: ignore  # noqa: PGH003
+    ]
 
     operations = [
-        migrations.CreateModel(
-            name="ContentGitRepository",
-            fields=[
-                (
-                    "created",
-                    model_utils.fields.AutoCreatedField(
-                        default=django.utils.timezone.now,
-                        editable=False,
-                        verbose_name="created",
-                    ),
-                ),
-                (
-                    "modified",
-                    model_utils.fields.AutoLastModifiedField(
-                        default=django.utils.timezone.now,
-                        editable=False,
-                        verbose_name="modified",
-                    ),
-                ),
-                (
-                    "content_key",
-                    opaque_keys.edx.django.models.LearningContextKeyField(
-                        max_length=255, primary_key=True, serialize=False
-                    ),
-                ),
-                ("git_url", models.CharField(max_length=255)),
-                ("is_export_enabled", models.BooleanField(default=True)),
-            ],
-            options={
-                "verbose_name": "Content Git Repository",
-                "verbose_name_plural": "Content Git Repositories",
-                "ordering": ["-created"],
-            },
+        migrations.RenameModel(
+            old_name="CourseGitRepository",
+            new_name="ContentGitRepository",
         ),
     ]
