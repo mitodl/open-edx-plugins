dd9e047e541c1f2ab5b28fcc7ff3b1e1d68a7fe7
Asad Ali <asad.ali@arbisoft.com>
2026-03-09 14:42:05 +0500

    ruff

--- src/ol_openedx_auto_select_language/ol_openedx_auto_select_language/urls.py
+++ src/ol_openedx_auto_select_language/ol_openedx_auto_select_language/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
         CourseLanguageView.as_view(),
         name="ol_course_language",
     ),
-    # TODO: Remove the legacy endpoint in a future release after updating all clients to use the new endpoint.
+    # TODO: Remove the legacy endpoint in a  # noqa: FIX002, TD003, TD002
+    #  future release after updating all clients to use the new endpoint.
     re_path(
         rf"/course-translations/api/course-language/{settings.COURSE_KEY_PATTERN}$",
         CourseLanguageView.as_view(),
