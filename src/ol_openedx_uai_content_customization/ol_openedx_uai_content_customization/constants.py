81980915cd8d3d91538376053e4cb5b9c7953887
Asad Ali <asad.ali@arbisoft.com>
2026-05-21 18:42:37 +0500

    fix unintended const change

--- src/ol_openedx_uai_content_customization/ol_openedx_uai_content_customization/constants.py
+++ src/ol_openedx_uai_content_customization/ol_openedx_uai_content_customization/constants.py
@@ -1,7 +1,7 @@
 """Constants for ol-openedx-uai-content-customization plugin."""
 
 INDUSTRY_CODES = {
-    "Healthcare": "HC-test",
+    "Healthcare": "HC",
     "Finance": "F",
     "Energy": "E",
     "Original industry": "",
