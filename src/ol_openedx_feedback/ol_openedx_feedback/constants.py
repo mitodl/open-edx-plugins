93c95ef529428f37f285614e408fd0abb1812e09
zamanafzal <zamanafzal@gmail.com>
2026-06-15 12:21:56 +0500

    refactor(feedback): trim dead constants + course-title util

--- src/ol_openedx_feedback/ol_openedx_feedback/constants.py
+++ src/ol_openedx_feedback/ol_openedx_feedback/constants.py
@@ -2,6 +2,3 @@
 
 # Structural/container blocks never get a feedback trigger; everything else does.
 EXCLUDED_BLOCK_TYPES = {"course", "chapter", "sequential", "vertical"}
-
-# Tracking event name emitted on each submission (flows to the data platform).
-FEEDBACK_SUBMITTED_EVENT = "edx.block.feedback.submitted"
