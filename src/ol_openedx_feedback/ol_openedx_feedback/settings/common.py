4ed32f455d30ec7604b8c20cb5388aabbe664934
zamanafzal <zamanafzal@gmail.com>
2026-06-15 12:21:30 +0500

    refactor(feedback): remove edx-platform persistence (data now in learn-ai)

--- src/ol_openedx_feedback/ol_openedx_feedback/settings/common.py
+++
@@ -1,7 +0,0 @@
-"""Settings to provide to edX"""
-
-
-def plugin_settings(settings):
-    """Populate common settings for ol_openedx_feedback."""
-    # Maximum length allowed for a feedback comment.
-    settings.OL_FEEDBACK_COMMENT_MAX_LENGTH = 1000
