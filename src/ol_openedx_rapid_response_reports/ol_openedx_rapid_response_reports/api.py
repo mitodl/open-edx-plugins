6ea44280f04e564406d42b6e2a9c1d123a42d563
Muhammad Anas <muhammad.anas@arbisoft.com>
2026-06-17 16:32:03 +0500

    WIP: Instructor dashbaord MFE APIs

--- src/ol_openedx_rapid_response_reports/ol_openedx_rapid_response_reports/api.py
+++ src/ol_openedx_rapid_response_reports/ol_openedx_rapid_response_reports/api.py
@@ -1,7 +1,46 @@
+from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from lms.djangoapps.instructor.permissions import VIEW_DASHBOARD
 from lms.djangoapps.instructor.views.api import require_course_permission
 from lms.djangoapps.instructor_analytics import csvs
+from opaque_keys.edx.keys import CourseKey
+
+
+@ensure_csrf_cookie
+@require_course_permission(VIEW_DASHBOARD)
+def list_rapid_response_runs(
+    request,  # noqa: ARG001
+    course_id,
+):
+    """Return JSON list of rapid response runs for the given course."""
+    from rapid_response_xblock.utils import get_run_data_for_course  # noqa: PLC0415
+
+    from ol_openedx_rapid_response_reports.utils import (  # noqa: PLC0415
+        get_display_name_from_usage_key,
+    )
+
+    course_key = CourseKey.from_string(course_id)
+    runs = get_run_data_for_course(course_key=course_key)
+
+    # Lazy import to avoid circular dependency at module level
+    from lms.djangoapps.courseware.courses import get_course_by_id  # noqa: PLC0415
+
+    course = get_course_by_id(course_key, depth=None)
+
+    return JsonResponse(
+        [
+            {
+                "id": str(run["id"]),
+                "problem_usage_key": str(run["problem_usage_key"]),
+                "problem_display_name": get_display_name_from_usage_key(
+                    run["problem_usage_key"], course
+                ),
+                "created": run["created"].isoformat(),
+            }
+            for run in runs
+        ],
+        safe=False,
+    )
 
 
 @ensure_csrf_cookie
